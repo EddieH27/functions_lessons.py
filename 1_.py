6f0bb7b800e5d57b2b5735b1bef0df7e92f421ab
EddieH27 <141260227+EddieH27@users.noreply.github.com>
2026-01-26 18:49:12 +0000

    l

--- 1_.py
+++ 1_.py
@@ -1,9 +1,43 @@
 # Methods, Help & Documentation Practice #1
 # Remove the characters to the left of our main text:
+# def happy_birthday(name, age):
+#  print(f"Happy birthday to {name}!")
+#  print(f"you are {age}")
+#  print("Happy birthday to you!")
+#  print()
+# happy_birthday("bro, 20")
+# happy_birthday("steve, 30")
+# happy_birthday("joe, 40")
 
+# def display_invoice(username, amount, due_date)
+#    print(f"Hello {username}")
+#    print(f"Your bill of ${amount:.2f} is due: {due_date}")
+# display_invoice("JoeSchmo", 100.01, "01/02") 
 # ,
+# def add(x,y):
+#     z= x + y
+#     return z
+# def subtract(x, y):
+#     z = x - y 
+#     return z
+# def multiply(x, y):
+#     z = x * y 
+#     return z
+# def Divide(x, y):
+#     z = x / y 
+#     return z
+# print(add(1, 2))
+# print(subtract(1, 2))
+# print(multiply(1, 2))
+# print(Divide(1, 2))
 
 # :
+def create_name(first, last):
+    first = first.capitalize()
+    last = last.capitalize()
+    return first + ' ' + last
+full_name = create_name("spongebob", "squarepants")
+print(full_name)
 
 # %
 
